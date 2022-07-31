ec1f9b985146cd7151458a0362bb91b9664cf623
DylanFromSP <92902711+DylanFromSP@users.noreply.github.com>
2022-07-31 18:49:19 +0800

    testing

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
 
+const PORT = process.env.PORT || 3001;
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -31,6 +33,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
