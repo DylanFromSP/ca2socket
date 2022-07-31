cf5ebbfcd5a29322cac748b07880d457b306b2fa
DylanFromSP <92902711+DylanFromSP@users.noreply.github.com>
2022-07-31 14:55:07 +0800

    Changing backend

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const express = require("express");
-const socket = require("socket.io");
 const app = express();
+const http = require("http");
 const cors = require("cors");
-
+const { Server } = require("socket.io");
 app.use(cors());
-app.use(express.json());
 
-const server = app.listen("3002", () => {
-  console.log("Server Running on Port 3002...");
-});
+const server = http.createServer(app);
 
-io = socket(server, {
+const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000"
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
-    console.log("User Joined Room: " + data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
   socket.on("send_message", (data) => {
-    console.log(data);
-    socket.to(data.room).emit("receive_message", data.content);
+    socket.to(data.room).emit("receive_message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("USER DISCONNECTED");
+    console.log("User Disconnected", socket.id);
   });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
 });
